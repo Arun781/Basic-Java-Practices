52c0f6da51143a3e6b94b7615c1bf043e5279368
arunbv0703 <113163145+arunbv0703@users.noreply.github.com>
2022-09-23 08:33:08 +0530

    Update CheifMinisterRunner.java

--- CheifMinisterRunner.java
+++ CheifMinisterRunner.java
@@ -3,27 +3,37 @@ class CheifMinisterRunner{
 public static void main(String[] values)
 {
         System.out.println("main : Infomation Of Chief Minister");
-        
+        System.out.println("Name of the Chief Minister:-");
         String ref0=values[0];
 		System.out.println("index 0 :" +ref0);
+		System.out.println("Age of the Chief Minister:-");
 		String ref1=values[1];
 		System.out.println("index 1 :" +ref1);
+		System.out.println("Place of the Chief Minister:-");
 		String ref2=values[2];
 		System.out.println("index 2 :" +ref2);
+		System.out.println("DOB of the Chief Minister:-");
 		String ref3=values[3];
 		System.out.println("index 3 :" +ref3);
+		System.out.println("Party of the Chief Minister:-");
 		String ref4=values[4];
 		System.out.println("index 4 :" +ref4);
+		System.out.println("State of the Chief Minister:-");
 		String ref5=values[5];
 		System.out.println("index 5 :" +ref5);
+		System.out.println("Elgibility of the Chief Minister:-");
 		String ref6=values[6];
 		System.out.println("index 6 :" +ref6);
+		System.out.println("Roll of the Chief Minister:-");
 		String ref7=values[7];
 		System.out.println("index 7 :" +ref7);
+		System.out.println("Salary of the Chief Minister:-");
 		String ref8=values[8];
 		System.out.println("index 8 :" +ref8);
+		System.out.println("Qualification of the Chief Minister:-");
 		String ref9=values[9];
 		System.out.println("index 9 :" +ref9);
+		System.out.println("Gender of the Chief Minister:-");
 		String ref10=values[10];
 		System.out.println("index 10 :" +ref10);
         
