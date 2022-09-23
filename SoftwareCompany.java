9ce6b2a190bbffd42b6daab2f7ab629dfbbe47c5
arunbv0703 <113163145+arunbv0703@users.noreply.github.com>
2022-09-23 08:34:59 +0530

    Update SoftwareCompany.java

--- SoftwareCompany.java
+++ SoftwareCompany.java
@@ -4,26 +4,47 @@ public static void main(String[] values)
 {
         System.out.println("main : SoftwareCompany");
         
-        String ref0=values[0];
+		System.out.println("Name of the SoftwareCompany");
+                String ref0=values[0];
 		System.out.println("index 0 :" +ref0);
+	
+		System.out.println("Location of the SoftwareCompany");
 		String ref1=values[1];
 		System.out.println("index 1 :" +ref1);
+	
+		System.out.println("Staff of the SoftwareCompany");
 		String ref2=values[2];
 		System.out.println("index 2 :" +ref2);
+	
+		System.out.println("NUm of floors of the SoftwareCompany");
 		String ref3=values[3];
 		System.out.println("index 3 :" +ref3);
+	
+		System.out.println("Num of employes of the SoftwareCompany");
 		String ref4=values[4];
 		System.out.println("index 4 :" +ref4);
+	
+		System.out.println("Start of date of the SoftwareCompany");
 		String ref5=values[5];
 		System.out.println("index 5 :" +ref5);
+	
+		System.out.println("GST num of the SoftwareCompany");
 		String ref6=values[6];
 		System.out.println("index 6 :" +ref6);
+	
+		System.out.println("Age of the SoftwareCompany");
 		String ref7=values[7];
 		System.out.println("index 7 :" +ref7);
+	
+		System.out.println("Completed years of the SoftwareCompany");
 		String ref8=values[8];
 		System.out.println("index 8 :" +ref8);
+	
+		System.out.println("Feedback about SoftwareCompany");
 		String ref9=values[9];
 		System.out.println("index 9 :" +ref9);
+	
+		System.out.println("Pan Num of the SoftwareCompany");
 		String ref10=values[10];
 		System.out.println("index 10 :" +ref10);
         
